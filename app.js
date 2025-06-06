97c00974d2483392786dda4951e826712d669a12
Deepanshu Thakur <86187639+digrajthakur@users.noreply.github.com>
2025-06-06 14:25:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,25 +34,6 @@ if (window.innerWidth > 768) {
 }
 
 
-// const faders = document.querySelectorAll('.fade-in');
-
-// const appearOptions = {
-//     threshold: 0.8
-// };
-
-// const appearOnScroll = new IntersectionObserver((entries, observer) => {
-//     entries.forEach(entry => {
-//         if (!entry.isIntersecting) return;
-//         entry.target.classList.add('visible');
-//         observer.unobserve(entry.target);
-//     });
-// }, appearOptions);
-
-// faders.forEach(fader => {
-//     appearOnScroll.observe(fader);
-// });
-
-
 const image = document.getElementById('caseImage');
 const section = document.querySelector('.sticky-section');
 
